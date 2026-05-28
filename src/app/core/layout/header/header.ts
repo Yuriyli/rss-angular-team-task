9773dab677262b83250bf73f528d6298a2e682a5
Yuriy Li <70540931+Yuriyli@users.noreply.github.com>
2026-05-28 18:34:23 +0300

    feat: header improvements (#55)
    * refactor: rename nav to nav-element
    
    * fix: missed property binding
    
    * feat: restyle nav-element
    
    * feat: add active status to nav-element

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Nav } from './components/nav-element';
+import { NavElement } from './components/nav-element';
 
 @Component({
   selector: 'app-header',
-  imports: [Nav],
+  imports: [NavElement],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
